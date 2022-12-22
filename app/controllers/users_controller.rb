ff9a1f83e79f62da797b2f7ddcd4206a1f72c0d9
EC2 Default User <ec2-user@ip-172-31-1-184.ap-northeast-1.compute.internal>
2022-12-22 11:55:18 +0000

    [finish]ouyou-2 debug bookers2 RSpec clear

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+class UsersController < ApplicationController
+  before_action:login_user_matching, only:[:edit, :update]
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+  end
+
+  def index
+    @users = User.all
+    @book = Book.new
+  end
+
+  def edit
+    @user=User.find(params[:id])
+
+  end
+
+  def update
+    @user=User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice]="You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def login_user_matching
+    user_id = params[:id].to_i
+    login_user_id = current_user.id
+    if(user_id != login_user_id)
+
+     redirect_to user_path(login_user_id)
+    end
+  end
+end
